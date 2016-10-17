009ce48e4275d7937fa675367fb1f3bc4b205a1b
Dilum Darshana <dilum.dar@gmail.com>
2016-10-17 19:10:46 +0530

    Few changes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+Route::group(['prefix' => 'v1'], function() {
+
+    Route::resource('meeting', 'MeetingController', [
+        'except' => ['']
+    ]);
+});
+
+Route::group(['prefix' => 'api/v1'], function() {
+
+    Route::post('user', [
+        'uses' => 'AuthController@store';
+    ]);
+});
+
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/user/{id?}', function($id = 4)
+{
+    return 'hellooo '.$id;
 });
